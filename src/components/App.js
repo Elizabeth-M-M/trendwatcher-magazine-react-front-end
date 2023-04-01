f895d71517c72814a25b8e84856fd0fa7df5e9bb
Elizabeth <mwendemuthusi6@gmail.com>
2023-04-01 13:47:59 +0300

    Added an image to test deployment

--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,13 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>App</div>
-  )
+    <div>
+      <img
+        src="https://images.pexels.com/photos/14656105/pexels-photo-14656105.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+        alt=""
+      />
+    </div>
+  );
 }
 
 export default App
